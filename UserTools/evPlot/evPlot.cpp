cf925401a0a3f78953a421e2f0bbdccc78455587
Jack Fannon <jackepfannon@gmail.com>
2023-09-26 13:38:10 +0100

    event plotter

--- UserTools/evPlot/evPlot.cpp
+++ UserTools/evPlot/evPlot.cpp
@@ -1,29 +1,100 @@
 #include "evPlot.h"
 
-evPlot::evPlot():Tool(){}
+evPlot::evPlot() : Tool() {}
 
+bool evPlot::Initialise(std::string configfile, DataModel &data) {
 
-bool evPlot::Initialise(std::string configfile, DataModel &data){
+  if (configfile != "")
+    m_variables.Initialise(configfile);
+  // m_variables.Print();
 
-  if(configfile!="")  m_variables.Initialise(configfile);
-  //m_variables.Print();
+  m_data = &data;
+  m_log = m_data->Log;
 
-  m_data= &data;
-  m_log= m_data->Log;
+  // Get the config variables: TreeReader, which plots to create, output
+  // directory, etc.
+  m_variables.Get("treeReaderName", treeReaderName);
+  m_variables.Get("outputDir", outputDir);
 
-  if(!m_variables.Get("verbose",m_verbose)) m_verbose=1;
+  // Get the TreeReader
+  if (m_data->Trees.count(treeReaderName) == 0) {
+    Log("ERROR: TreeReader " + treeReaderName + " not found!", v_error,
+        verbosity);
+    return false;
+  } else {
+    myTreeReader = m_data->Trees.at(treeReaderName);
+  }
+
+  if (!m_variables.Get("verbose", m_verbose))
+    m_verbose = 1;
+
+  // Initialise the histograms
+  hitTimes = new TH1D("hitTimes", "Hit Times", 100, 0, 1500);
+  hitTimes->SetLineColor(38);
+  hitTimes->SetFillColor(38);
+  hitTimes->GetXaxis()->SetTitle("Time (ns)");
+
+  hitCharges = new TH1D("hitCharges", "Hit Charges", 100, 0, 1100);
+  hitCharges->SetLineColor(46);
+  hitCharges->SetFillColor(46);
+  hitCharges->GetXaxis()->SetTitle("Charge (pC)");
+
+  hitTimesAndCharges = new TH2D("hitTimesAndCharges", "Hit Times and Charges",
+                                150, 0, 1500, 110, 0, 1100);
+  hitTimesAndCharges->GetXaxis()->SetTitle("Time (ns)");
+  hitTimesAndCharges->GetYaxis()->SetTitle("Charge (pC)");
+
+  /* Create the canvas and divide it up
+    -------------------
+   | ----------------- |
+   ||     HEATMAP     ||
+   ||     T vs Q      ||
+   | ----------------- |
+   | -------   ------- |
+   ||  TIME | | CHRGE ||
+   | -------   ------- |
+    -------------------
+  */
+
+  plotCanvas = new TCanvas("displayCanvas", "Display Canvas", 1920, 1080);
+  plotCanvas->Divide(1, 2);
+  plotPad = plotCanvas->cd(2);
+  plotPad->Divide(2, 1);
+  plotCanvas->cd(2);
 
   return true;
 }
 
+bool evPlot::Execute() {
 
-bool evPlot::Execute(){
+  // Get the number of hit PMTs from the TQReal branch
+  totalPMTsHit = myTQReal->cables.size();
+  if (totalPMTsHit == 0) {
+    std::cout << "No PMTs hit in this event!" << std::endl;
+    return true;
+  }
+
+  // Loop over the hit PMTs and fill the histograms, call the iterator pmtNumber
+  for (int pmtNumber = 0; pmtNumber < totalPMTsHit; ++pmtNumber) {
+    cableNumber = myTQReal->cables.at(pmtNumber);
+    charge = myTQReal->Q.at(pmtNumber);
+    time = myTQReal->T.at(pmtNumber);
+
+    // Fill the histograms
+    hitTimes->Fill(time);
+    hitCharges->Fill(charge);
+    hitTimesAndCharges->Fill(time, charge);
+  }
+
+  // Draw the histograms
+  plotPad->cd(1);
+  hitTimes->Draw();
+  plotPad->cd(2);
+  hitCharges->Draw();
+  plotCanvas->cd(1);
+  hitTimesAndCharges->Draw("COLZ");
 
   return true;
 }
 
-
-bool evPlot::Finalise(){
-
-  return true;
-}
+bool evPlot::Finalise() { return true; }
--- UserTools/evPlot/evPlot.h
+++ UserTools/evPlot/evPlot.h
@@ -1,39 +1,74 @@
 #ifndef evPlot_H
 #define evPlot_H
 
-#include <string>
 #include <iostream>
+#include <string>
 
+#include "TCanvas.h"
+#include "TH1.h"
+#include "TH2.h"
 #include "Tool.h"
 
+#include "skroot.h"
 
 /**
  * \class evPlot
  *
- * This is a balnk template for a Tool used by the script to generate a new custom tool. Please fill out the descripton and author information.
-*
-* $Author: B.Richards $
-* $Date: 2019/05/28 10:44:00 $
-*/
+ * This is a balnk template for a Tool used by the script to generate a new
+ * custom tool. Please fill out the descripton and author information.
+ *
+ * $Author: B.Richards $
+ * $Date: 2019/05/28 10:44:00 $
+ */
 
-class evPlot: public Tool {
+class evPlot : public Tool {
 
-
- public:
-
+public:
   evPlot(); ///< Simple constructor
-  bool Initialise(std::string configfile,DataModel &data); ///< Initialise Function for setting up Tool resorces. @param configfile The path and name of the dynamic configuration file to read in. @param data A reference to the transient data class used to pass information between Tools.
-  bool Execute(); ///< Executre function used to perform Tool perpose. 
+  bool Initialise(
+      std::string configfile,
+      DataModel
+          &data); ///< Initialise Function for setting up Tool resorces. @param
+                  ///< configfile The path and name of the dynamic configuration
+                  ///< file to read in. @param data A reference to the transient
+                  ///< data class used to pass information between Tools.
+  bool Execute(); ///< Executre function used to perform Tool perpose.
   bool Finalise(); ///< Finalise funciton used to clean up resorces.
 
+  // Tool config
+  std::string treeReaderName;
+  std::string outputDir;
+  bool plotHitTimes;
+  bool plotHitCharges;
+  bool plotHTandC;
 
- private:
+  // Tree reader
+  MTreeReader *myTreeReader = nullptr;
 
+  // Canvas and pads
+  TCanvas *plotCanvas = nullptr;
+  TVirtualPad *plotPad = nullptr;
 
+  // Histograms
+  TH1D *hitTimes = nullptr;
+  TH1D *hitCharges = nullptr;
+  TH2D *hitTimesAndCharges = nullptr;
 
+  // Branches
+  const Header *myHeader = nullptr;
+  const TQReal *myTQReal = nullptr;
 
+  // Plotting variables
+  int totalPMTsHit = -1;
+  int cableNumber;
+  int charge;
+  float time;
 
+  // Verbosity
+  int verbosity = 1;
+  int v_error = 0;
+
+private:
 };
-
 
 #endif
